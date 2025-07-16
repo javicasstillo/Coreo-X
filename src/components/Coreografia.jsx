9e932b6aedef96b5c75895f5e768ed0bd713501c
javier <javiercastillo.tuc@gmail.com>
2025-07-16 20:56:48 -0300

    subir

--- src/components/Coreografia.jsx
+++ src/components/Coreografia.jsx
@@ -5,6 +5,15 @@ function Coreografia(){
     
     const [hora, setHora] = useState(0)
     const [minuto, setMinuto] = useState(0)
+    const [usuarios, setUsuarios] = useState({
+        usuario1: "",
+        usuario2: "",
+        usuario3: "",
+        usuario4: "",
+        usuario5: "",
+        usuario6: "",
+        usuario7: "",
+    })
 
     useEffect(() => {
         const intervalo = setInterval(() => {
@@ -16,88 +25,87 @@ function Coreografia(){
         return () => clearInterval(intervalo)
     }, [])
 
-    let usuario1 = "Florencia" 
-    let usuario2 = "Javier" 
-    let usuario3 = "Kevin" 
-    let usuario4 = "Luciano" 
-    let usuario5 = "Carlos" 
-    let usuario6 = "Luciana" 
-    let usuario7 = "Ludmila" 
+    useEffect(()=>{
+        condicionLogica()
+    }, [hora, minuto])
 
-    if (hora > 13 || (hora === 13 && minuto > 50)) {
-        usuario1 = "Carlos"
-        usuario2 = "Luciano"
-        usuario3 = "Kevin" 
-        usuario4 = "Javier" 
-        usuario5 = "Florencia" 
-        usuario6 = "Luciana" 
-        usuario7 = "Ludmila" 
-    }  else if(hora >12 || (hora === 12 && minuto > 30)){
-        usuario1 = "Kevin" 
-        usuario2 = "Ludmila" 
-        usuario3 = "Kevin" 
-        usuario4 = "Luciano" 
-        usuario5 = "Carlos" 
-        usuario6 = "Luciana" 
-        usuario7 = "Ludmila"
-    } else if(hora >11 ||(hora === 11 && minuto >10) ){
-        usuario1 = "Florencia" 
-        usuario2 = "Javier" 
-        usuario3 = "Kevin" 
-        usuario4 = "Luciano" 
-        usuario5 = "Carlos" 
-        usuario6 = "Luciana" 
-        usuario7 = "Ludmila"
-    } else if(hora > 9 || (hora === 9 && minuto > 50)){
-        usuario1 = "Kevin" 
-        usuario2 = "Ludmila" 
-        usuario3 = "Kevin" 
-        usuario4 = "Luciano" 
-        usuario5 = "Carlos" 
-        usuario6 = "Luciana" 
-        usuario7 = "Ludmila"
-    } else if (hora < 9 || (hora === 9 && minuto < 50)){
-        usuario1 = "Florencia" 
-        usuario2 = "Javier" 
-        usuario3 = "Kevin" 
-        usuario4 = "Luciano" 
-        usuario5 = "Carlos" 
-        usuario6 = "Luciana" 
-        usuario7 = "Ludmila"
+    const grupo1 = () => {
+    setUsuarios({
+      usuario1: "Florencia",
+      usuario2: "Javier",
+      usuario3: "Kevin",
+      usuario4: "Luciano",
+      usuario5: "Carlos",
+      usuario6: "Ludmila",
+    });
+  };
+
+  const grupo2 = () => {
+    setUsuarios({
+      usuario1: "Kevin",
+      usuario2: "Ludmila",
+      usuario3: "Florencia",
+      usuario4: "Luciano",
+      usuario5: "Carlos",
+      usuario6: "Javier",
+    });
+  };
+
+  const grupo3 = () => {
+    setUsuarios({
+      usuario1: "Carlos",
+      usuario2: "Luciano",
+      usuario3: "Javier",
+      usuario4: "Florencia",
+      usuario5: "Ludmila",
+    });
+}
+   
+    const condicionLogica = ()=>{
+        if (hora > 13 || (hora === 13 && minuto > 50)) {
+            grupo3()
+        }  else if(hora >12 || (hora === 12 && minuto > 30)){
+            grupo2()
+        } else if(hora >11 ||(hora === 11 && minuto >10) ){
+            grupo1()
+        } else if(hora > 9 || (hora === 9 && minuto > 50)){
+            grupo2()
+        } else if (hora > 8 || (hora === 8 && minuto > 29)){
+            grupo1()
+        }
     }
 
     return <>
         <nav className="py-3 bg-body-tertiary">
-            <h3 className="text-center marca">Coreo X</h3>
+            <h3 className="text-center marca fs-1">Coreo X</h3>
         </nav>
 
         <main>
             <section id="coreo">
                 <div className="container">
-                    <div className="row py-3 justify-content-center">
+                    <div className="row gy-3 py-3 justify-content-center">
                         <div className="col-12 col-md-6">
                             <div className="card h-100 align-items-center">
                                 <div className="fondo1 w-100 d-flex align-items-center justify-content-center">
-                                    <h3 className="fs-1 text-white">Derivando</h3>
+                                    <h3 className="fs-1 text-white marca">Derivando</h3>
                                 </div>
-                                <ul className="list-group py-3">
-                                    <li className="list-group-item">{usuario1}</li>
-                                    <li className="list-group-item">{usuario2}</li>
-                                </ul>
+                                <div className=" d-flex gap-3 my-auto py-3">
+                                    <p className="mb-0 p-2  rounded bg-secondary-subtle">{usuarios.usuario1}</p>
+                                    <p className="mb-0 p-2  rounded bg-secondary-subtle">{usuarios.usuario2}</p>
+                                </div>
                             </div>
                         </div>
                         <div className="col-12 col-md-6">
                             <div className="card h-100 align-items-center">
                                 <div className="fondo2 w-100 d-flex align-items-center justify-content-center">
-                                    <h3 className="fs-1 text-white">Atendiendo</h3>
+                                    <h3 className="fs-1 text-white marca">Atendiendo</h3>
                                 </div>
-                                <ul className="list-group py-3">
-                                    <li className="list-group-item">{usuario3}</li>
-                                    <li className="list-group-item">{usuario4}</li>
-                                    <li className="list-group-item">{usuario5}</li>
-                                    <li className="list-group-item">{usuario6}</li>
-                                    <li className="list-group-item">{usuario7}</li>
-                                </ul>
+                                <div className=" d-flex gap-3 my-auto py-3">
+                                    <p className="mb-0 p-2  rounded bg-warning-subtle">{usuarios.usuario3}</p>
+                                    <p className="mb-0 p-2  rounded bg-warning-subtle">{usuarios.usuario4}</p>
+                                    <p className="mb-0 p-2  rounded bg-warning-subtle">{usuarios.usuario5}</p>
+                                    <p className="mb-0 p-2  rounded bg-warning-subtle">{usuarios.usuario6}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
