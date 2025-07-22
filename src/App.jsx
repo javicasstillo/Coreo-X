e1f2e355f9e147959507002c29d10b93292a0de5
javier <javiercastillo.tuc@gmail.com>
2025-07-22 08:58:22 -0300

    listo

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import Coreografia from './components/Coreografia'
-import Despertador from './components/Despertador';
-
-import { useEffect, useState } from "react";
 
 function App() {
-    const [mostrarDespertador, setMostrarDespertador] = useState(true);
-
-    useEffect(() => {
-        const ahora = new Date();
-        const hora = ahora.getHours();
-        const minutos = ahora.getMinutes();
-        
-        const esAntesDe830 = hora < 8 || (hora === 8 && minutos < 30);
-
-        setMostrarDespertador(esAntesDe830);
-    }, []);
-
-    return (
-        <div>
-            {mostrarDespertador ? <Despertador /> : <Coreografia />}
-        </div>
-    );
+    return <div>
+        <Coreografia></Coreografia>
+    </div>
 }
 
 export default App;
